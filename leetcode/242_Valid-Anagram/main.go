089d4df7828fc7e07fcafb225066b41b78f189ad
ThiccPan <ihilmiderian@gmail.com>
2023-06-28 19:52:05 +0700

    242: initial submit

---
+++ leetcode/242_Valid-Anagram/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// my first try
+func isAnagram(s string, t string) bool {
+    strMap := map[rune]int{}
+	
+	if len(s) != len(t) {
+		return false
+	}
+
+	// map every rune in str
+	for idx, strRune := range s {
+		strMap[strRune]++
+		strMap[rune(t[idx])]--
+	}
+
+	// check if any rune is occuring odd times
+	// if so then not anagram
+	for _, v := range strMap {
+		if v % 2 != 0 || v < 0{
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	first := isAnagram("hello", "eollh")
+	fmt.Println(first)
+}
