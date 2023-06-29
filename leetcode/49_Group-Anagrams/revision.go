30482b4ddfd0c67bf5f12da9d31d273836b914de
ThiccPan <ihilmiderian@gmail.com>
2023-06-29 11:59:22 +0700

    lc: 49 revision 1 (without sorting)

---
+++ leetcode/49_Group-Anagrams/revision.go
@@ -0,0 +1,25 @@
+package main
+
+type Key [26]byte
+
+func strKey(str string) (key Key) {
+	for v := range str {
+		key[str[v]-'a']++
+	}
+	return key
+}
+
+func groupAnagramsRevision(strs []string) [][]string {
+	subStrMap := make(map[Key][]string)
+	subStrs := [][]string{}
+
+	for _, str := range strs {
+		key := strKey(str)
+		subStrMap[key] = append(subStrMap[key], str)
+	}
+
+	for _, v := range subStrMap {
+		subStrs = append(subStrs, v)
+	}
+	return subStrs
+}
